2adaf8d8f8381e5c24864c5b1c7496b0b3a845c1
Tommy <thie7042@uni.sydney.edu.au>
2022-03-28 11:30:09 +1100

    Creating GP python file

--- Gaussian Process.py
+++ Gaussian Process.py
@@ -1,7 +1,13 @@
 import matplotlib.pyplot as plt
 
-import gpflow
-import gpflowopt
+
 import numpy as np
+
+
+import tensorflow as tf
+
+np.random.seed(1793)
+tf.random.set_seed(1793)
+
 
 print("Hello world")
