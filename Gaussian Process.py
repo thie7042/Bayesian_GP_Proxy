fb9ebc10e36349ef0dcb1e465e580e296aedf9f5
Tommy <thie7042@uni.sydney.edu.au>
2022-03-28 17:24:32 +1100

    Creating Visualisation & Ask-Tell Tools

--- Gaussian Process.py
+++ Gaussian Process.py
@@ -1,22 +1,37 @@
-import matplotlib.pyplot as plt
-import math
 import numpy as np
-import pandas as pd
-import plotly.express as px
-import cufflinks as cf
-import plotly.graph_objects as go
-import plotly.express as px
 import tensorflow as tf
+import pickle
+
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import gpflow
+
+from trieste.ask_tell_optimization import AskTellOptimizer
+from trieste.bayesian_optimizer import Record
+from trieste.data import Dataset
+from trieste.models.gpflow.models import GaussianProcessRegression
+from trieste.objectives import scaled_branin, SCALED_BRANIN_MINIMUM
+from trieste.objectives.utils import mk_observer
+from trieste.space import Box
+
+
+from trieste.models.gpflow import GaussianProcessRegression
 
 
 from skopt.benchmarks import branin as branin
-from skopt.benchmarks import hart6 as hart6_
+
 
 from matplotlib import pyplot as plt
 from matplotlib.colors import LogNorm
 
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+from trieste.space import Box
+
+search_space = Box([0, 0], [1, 1])  # define the search space directly. We are looking in a 2d plane (2-dimensonal, 2 valriables ranging 0-1)
+
+
+
+
 
 np.random.seed(1793)
 tf.random.set_seed(1793)
@@ -30,21 +45,12 @@ from trieste.objectives import (
     SCALED_BRANIN_MINIMUM,
     BRANIN_SEARCH_SPACE,
 )
-from trieste.objectives.utils import mk_observer
-
-from trieste.space import Box
 
 
 
-
-
-search_space = BRANIN_SEARCH_SPACE  # predefined search space, for convenience
-search_space = Box([0, 0], [1, 1])  # define the search space directly
-
-
-
-##############
+############## Plotting of benchmark function. This is just for context ##########
 def plot_branin():
+
     fig, ax = plt.subplots()
     #fig, (ax, ax1) = plt.subplots(1, 2, figsize=(20, 8))
 
@@ -52,6 +58,8 @@ def plot_branin():
     x2_values = np.linspace(0, 15, 100)
     x_ax, y_ax = np.meshgrid(x1_values, x2_values)
     vals = np.c_[x_ax.ravel(), y_ax.ravel()]
+
+    # Define the benchmark function
     fx = np.reshape([branin(val) for val in vals], (100, 100))
 
     cm = ax.pcolormesh(x_ax, y_ax, fx,
@@ -77,23 +85,165 @@ def plot_branin():
     ax1 = plt.axes(projection='3d')
     ax1.plot_surface(x_ax, y_ax, fx, cmap='jet')
 
-
     plt.show()
 
-
-
 plot_branin()
-plt.show()
-
-#######
 
 
+###########
 
-"""fig = plot_function_plotly(
-    scaled_branin, search_space.lower, search_space.upper, grid_density=20
-)
-fig.update_layout(height=400, width=400)
-fig.show()"""
+
+import trieste
+from trieste.models.gpflow import build_gpr
+
+"""observer = trieste.objectives.utils.mk_observer(scaled_branin)
+
+num_initial_points = 5
+initial_query_points = search_space.sample_sobol(num_initial_points)
+initial_data = observer(initial_query_points)"""
+
+#TEST#\/\/\/\/\/
+def build_model(data, kernel_func=None):
+    """kernel_func should be a function that takes variance as a single input parameter"""
+    variance = tf.math.reduce_variance(data.observations)
+    if kernel_func is None:
+        kernel = gpflow.kernels.Matern52(variance=variance)
+    else:
+        kernel = kernel_func(variance)
+    gpr = gpflow.models.GPR(data.astuple(), kernel, noise_variance=1e-5)
+    gpflow.set_trainable(gpr.likelihood, False)
+
+    return GaussianProcessRegression(gpr)
+
+
+num_initial_points = 5
+observer = trieste.objectives.utils.mk_observer(scaled_branin)
+initial_query_points = search_space.sample_sobol(num_initial_points)
+initial_data = observer(initial_query_points)
+n_steps = 5
+model = build_model(initial_data)
+ask_tell = AskTellOptimizer(search_space, initial_data, model)
+for step in range(n_steps):
+    print(f"Ask Trieste for configuration #{step}")
+    new_config = ask_tell.ask()
+
+    print("Saving Trieste state to re-use later")
+    state: Record[None] = ask_tell.to_record()
+    saved_state = pickle.dumps(state)
+
+    print(f"In the lab running the experiment #{step}.")
+    new_datapoint = scaled_branin(new_config)
+
+    print("Back from the lab")
+    print("Restore optimizer from the saved state")
+    loaded_state = pickle.loads(saved_state)
+    ask_tell = AskTellOptimizer.from_record(loaded_state, search_space)
+    ask_tell.tell(Dataset(new_config, new_datapoint))
+
+
+####^^^^^^^
+
+
 
 print("______________________________________________________________")
-print("Hello world")
+print("Hello world! Let's Create our Proxy!")
+
+
+
+gpflow_model = build_gpr(initial_data, search_space, likelihood_variance=1e-7)
+model = GaussianProcessRegression(gpflow_model, num_kernel_samples=100)
+
+bo = trieste.bayesian_optimizer.BayesianOptimizer(observer, search_space)
+
+num_steps = 15
+result = bo.optimize(num_steps, initial_data, model)
+dataset = result.try_get_final_dataset()
+
+print("______________________________________________________________")
+
+
+query_points = dataset.query_points.numpy()
+observations = dataset.observations.numpy()
+
+arg_min_idx = tf.squeeze(tf.argmin(observations, axis=0))
+
+print(f"query point: {query_points[arg_min_idx, :]}")
+print(f"observation: {observations[arg_min_idx, :]}")
+
+
+print("___________________________VISUALIZE 2D___________________________________")
+
+
+x_pos_best = query_points[arg_min_idx, :][0]
+y_pos_best = query_points[arg_min_idx, :][1]
+z_pos_best = observations[arg_min_idx, :][0]
+
+
+print(query_points)
+print(observations)
+
+x_1 = []
+x_2 = []
+obs = []
+for i in range(query_points.shape[0]):
+    x_1.append(query_points[i][0])
+    x_2.append(query_points[i][1])
+    obs.append(observations[i][0])
+
+print(query_points)
+print(x_1)
+print(x_2)
+
+fig, ax = plt.subplots()
+plt.plot(x_1,x_2,'o', color='black')
+plt.plot(x_pos_best, y_pos_best,'o', color='red')
+
+#plot_bo_points(query_points, ax[0, 0], num_initial_points, arg_min_idx)
+ax.set_xlabel("$X_0$")
+ax.set_xlim([0, 1])
+ax.set_ylabel("$X_1$")
+ax.set_ylim([0, 1])
+
+
+#Contour
+
+def NormalizeData(data):
+    return (data - np.min(data)) / (np.max(data) - np.min(data))
+
+
+
+x1_values = np.linspace(-5, 10, 100)
+x2_values = np.linspace(0, 15, 100)
+x_ax, y_ax = np.meshgrid(x1_values, x2_values)
+vals = np.c_[x_ax.ravel(), y_ax.ravel()]
+fx = np.reshape([branin(val) for val in vals], (100, 100))
+
+fx = NormalizeData(fx)
+x_ax = NormalizeData(x1_values)
+y_ax = NormalizeData(x2_values)
+
+
+levels = np.arange(0,1,0.01)
+ax.contour(x_ax, y_ax, fx,levels=levels)
+#cb = fig.colorbar(cm)
+
+
+plt.show()
+
+
+print("___________________________VISUALIZE 3D___________________________________")
+
+
+
+obs = NormalizeData(obs)
+
+
+
+ax1 = plt.axes(projection='3d')
+levels = np.arange(0,1,0.01)
+ax1.contour(x_ax, y_ax, fx,levels=levels)
+
+plt.plot(x_1,x_2,obs,'o', color='black')
+plt.plot(x_pos_best, y_pos_best, z_pos_best,'o', color='red')
+
+plt.show()
